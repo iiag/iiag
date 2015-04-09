a2c8e987b90671b68ab12c95d12b46c0180d4930
Chris Harding <wablahflababablah@gmail.com>
2015-04-09 05:05:54 -0400

    Added missing headers

--- src/serve.c
+++ src/serve.c
@@ -1,7 +1,17 @@
 #include <assert.h>
+#include <errno.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+
 #include "serve.h"
+#include "proto.h"
 #include "util/list.h"
 
 struct server {
