d70844f1d22ec22bed72c885df1e3530d2325ffe
Chris Harding <wablahflababablah@gmail.com>
2015-04-10 07:52:55 -0400

    Update tests for list to be more robust and test new functions

--- tests/list/test.c
+++ tests/list/test.c
@@ -4,7 +4,7 @@
 
 #define expect(C) \
 	if (!(C)) { \
-		fprintf(stderr, "test failed: " #C); \
+		fprintf(stderr, "test failed on line %d: " #C "\n", __LINE__); \
 		return EXIT_FAILURE; \
 	}
 
@@ -22,6 +22,15 @@ void push_all(list_t * ls, int * ints, int cnt)
 	}
 }
 
+void push_all_back(list_t * ls, int * ints, int cnt)
+{
+	int i;
+
+	for (i = 0; i < cnt; i++) {
+		list_push_back(ls, ints + i);
+	}
+}
+
 int main()
 {
 	int sum = 0;
@@ -30,15 +39,43 @@ int main()
 	int stuff[] = {1, 2, 4, 100, 101};
 	int stuff2[] = {8, 16};
 
+	// l = []
+	expect(list_pop(l) == NULL);
+	expect(list_pop_back(l) == NULL);
+
+	list_push(l, stuff);
+	// l = [1]
+
+	expect(*(int *)list_pop(l) == 1);
+	expect(list_pop(l) == NULL);
+	// l = []
+
+	list_push_back(l, stuff);
+	// l = [1]
+
+	expect(*(int *)list_pop_back(l) == 1);
+	expect(list_pop_back(l) == NULL);
+	// l = []
+
 	push_all(l, stuff, 5);
+	// l = [101, 100, 4, 2, 1]
+
+	expect(list_size(l) == 5);
 
 	expect(*(int *)list_pop(l) == 101);
 	expect(*(int *)list_pop(l) == 100);
+	// l = [4, 2, 1]
 
-	push_all(l, stuff2, 2);
+	expect(list_size(l) == 3);
 
+	push_all_back(l, stuff2, 2);
+	// l = 4, 2, 1, 8, 16
+
+	list_foreach_reverse(l, &sum, (list_foreach_func)sumify);
+	expect(sum == 1 + 2 + 4 + 8 + 16);
+
+	sum = 0;
 	list_foreach(l, &sum, (list_foreach_func)sumify);
-
 	expect(sum == 1 + 2 + 4 + 8 + 16);
 
 	list_destroy(l);
