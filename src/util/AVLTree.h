2f0d6d5e5900c2d4b9e7275868d9d1533563f9b7
Lundar <Lundar@clarkson.edu>
2015-05-27 13:40:09 -0400

    styling

--- src/util/AVLTree.h
+++ src/util/AVLTree.h
@@ -9,29 +9,29 @@ typedef struct AVLNode_t AVLNode;
 0 =
 -1 <
 */
-typedef int (*comp_func)(void*, void*);
-typedef void (*map_func)(void*, void*);
+typedef int (*comp_func)(void *, void *);
+typedef void (*map_func)(void *, void *);
 
-AVLTree* avl_new(comp_func);
+AVLTree *avl_new(comp_func);
 /*
 Note avl_destroy will free data in nodes aswell
 */
-void avl_destroy(AVLTree*);
-void avl_add(AVLTree*,void*);
-void avl_delete(AVLTree*,void*);
-void avl_reroot(AVLTree*);
-void avl_map(AVLTree* t,void* arg, map_func);
-int  avl_height(AVLTree*);
-int  avl_size(AVLTree*);
-void* avl_find(AVLTree*,void*);
+void avl_destroy(AVLTree *);
+void avl_add(AVLTree *, void *);
+void avl_delete(AVLTree *, void *);
+void avl_reroot(AVLTree *);
+void avl_map(AVLTree *t, void *arg, map_func);
+int  avl_height(AVLTree *);
+int  avl_size(AVLTree *);
+void *avl_find(AVLTree *, void *);
 
-AVLNode* avl_new_node();
-void avl_destroy_node(AVLNode*);
-void avl_rotate_right(AVLNode*);
-void avl_rotate_left(AVLNode*);
-void avl_remeasure(AVLNode*);
-void avl_rebalance(AVLNode*);
-AVLNode* avl_node_find(AVLNode*,void*,comp_func fun);
+AVLNode *avl_new_node();
+void avl_destroy_node(AVLNode *);
+void avl_rotate_right(AVLNode *);
+void avl_rotate_left(AVLNode *);
+void avl_remeasure(AVLNode *);
+void avl_rebalance(AVLNode *);
+AVLNode *avl_node_find(AVLNode *, void *, comp_func fun);
 
 
 
--- src/util/map.h
+++ src/util/map.h
@@ -3,14 +3,14 @@
 
 typedef struct AVL_Map map_t;
 
-typedef void (*map_map_func)(int, void*, void*);
+typedef void (*map_map_func)(int, void *, void *);
 
-map_t* map_new();
-void   map_free(map_t*);
-void*  map_get(map_t*,int);
-void*  map_remove(map_t*,int);
-void   map_add(map_t*,int,void*);
-void   map_map(map_t*,map_map_func,void* data);
+map_t *map_new();
+void   map_free(map_t *);
+void  *map_get(map_t *, int);
+void  *map_remove(map_t *, int);
+void   map_add(map_t *, int, void *);
+void   map_map(map_t *, map_map_func, void *data);
 
 
 
